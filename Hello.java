a661a6c56f44db9a4cb61c2631ea3fb7a592f256
AhmadScruggs <112003991+AhmadScruggs@users.noreply.github.com>
2022-08-24 12:12:44 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/***********************************************************************
+* Java jdk
+************************************************************************
+Ahmad Scruggs
+8/24/2022
+CMSC255 004
+***********************************************************************/
+
+public class Hello{
+	public static void main(String[] args) {
+		System.out.println("<Ahmad Scruggs>");
+		System.out.println("<Java Jdk>");
+		System.out.println("<Lab 01>");
+		System.out.println("<08/24/2022>");
+		System.out.println("<CMSC255 004>");
+		System.out.println();
+
+}
+	}
